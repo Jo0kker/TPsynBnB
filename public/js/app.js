7d7dbde0ede3fc0327ee7a9dc8906499e414b53c
Jo0kker <boublou@hotmail.fr>
2019-08-11 22:02:07 +0200

    Ajout de webpack et encore pour le css

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+var $ = require('./jquery-3.3.1.slim.min.js');
+require('./popper.min.js');
+require('./bootstrap.min.js');
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,30 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
 
-    /**
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * @Route("/hello/{prenom}",name="hello_prenom")
-     * Montre la page qui dit bonjour
-     * @return Response
-     */
-    public function hello($prenom = 'anonyme', $age = 0)
-    {
-        return new Response('Bonjour '.$prenom.' vous avez '.$age);
-    }
-
 
     /**
      * @Route("/", name="homepage")
      */
-    public function home() {
+    public function home(AdRepository $adRepository, UserRepository $userRepository) {
         $prenom = ['Benjamin' => 23, 'Norbert' => 18, 'Andrée' => 33];
         return $this->render(
             'home.html.twig',
             [
-                'title' => 'Bonjour à tous',
-                'age' => 15,
-                'tab' => $prenom
+                'ads' => $adRepository->findBestAds(3),
+                'users' => $userRepository->findBestUsers(3)
             ]
         );
     }
